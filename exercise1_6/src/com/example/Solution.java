94c19001c488b42271c075c17cf9e1711c224a8b
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-07 16:15:12 +1200

    exercise 1.6 complete code

---
+++ exercise1_6/src/com/example/Solution.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();  //lines
+
+        int d,q,x,y;
+
+        ArrayList[] set = new ArrayList[n];
+
+        for(int i=0; i<n ; i++){
+
+            d = scan.nextInt();
+            set[i] = new ArrayList();
+            for(int j =0; j<d; j++){
+                set[i].add(scan.nextInt());
+            }
+        }
+
+        q = scan.nextInt();
+        for (int i=0; i<q; i++){
+               x = scan.nextInt();
+               y = scan.nextInt();
+
+               try {
+                   System.out.println(set[x - 1].get(y - 1));
+               } catch(Exception ex){
+                   System.out.println("ERROR!");
+               }
+        }
+
+
+
+
+
+
+
+    }
+}
